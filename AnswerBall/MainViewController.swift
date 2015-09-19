9ebc10649a5b6592e3fbdfefc2b1e12d0fcea5a0
Zenun-Vee <zenunvucetovic@gmail.com>
2015-09-19 14:54:12 -0400

    Added sound

--- AnswerBall/MainViewController.swift
+++ AnswerBall/MainViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, AVAudioPlayerDelegate {
 
     @IBOutlet weak var answerLabel: UILabel!
     
+    var audioPlayer = AVAudioPlayer()
     
     var answers = [
         "I'm not a mindreader!",
@@ -70,10 +72,52 @@ class MainViewController: UIViewController {
         
         answerLabel.text = "\(answers[random])"
         
+        do {
+            try playSound("shakesound", fileExtension: "wav")
+            
+        } catch {
+            return
+        }
+    }
+    
+    func playSound(fileName: String, fileExtension: String) throws {
         
+        let dispatchQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
         
+        dispatch_async(dispatchQueue, { let mainBundle = NSBundle.mainBundle()
+            
+            let filePath = mainBundle.pathForResource("\(fileName)", ofType:"\(fileExtension)")
+            
+            if let path = filePath{
+                let fileData = NSData(contentsOfFile: path)
+                
+                do {
+                    /* Start the audio player */
+                    self.audioPlayer = try AVAudioPlayer(data: fileData!)
+                    
+                    guard let player : AVAudioPlayer? = self.audioPlayer else {
+                        return
+                    }
+                    
+                    /* Set the delegate and start playing */
+                    player!.delegate = self
+                    if player!.prepareToPlay() && player!.play() {
+                        /* Successfully started playing */
+                    } else {
+                        /* Failed to play */
+                    }
+                    
+                } catch {
+                    //self.audioPlayer = nil
+                    return
+                }
+                
+            }
+            
+        })
         
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
